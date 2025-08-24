090b642f493b975f24dadd8f282e16eedd750279
Marcel Otto <marcelotto@gmx.de>
2025-08-24 15:54:19 +0200

    Initial commit
    $ mix new dcatr --module DCATR
    
    with Elixir v1.18.4

---
+++ lib/dcatr.ex
@@ -0,0 +1,18 @@
+defmodule DCATR do
+  @moduledoc """
+  Documentation for `DCATR`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DCATR.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dcatr_test.exs
@@ -0,0 +1,8 @@
+defmodule DCATRTest do
+  use ExUnit.Case
+  doctest DCATR
+
+  test "greets the world" do
+    assert DCATR.hello() == :world
+  end
+end
